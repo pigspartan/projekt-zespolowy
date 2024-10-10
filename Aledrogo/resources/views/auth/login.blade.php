9a03a13bc7a2efbe17e6c2ebcf133744a90109a3
Kross003 <161240538+Kross003@users.noreply.github.com>
2024-10-11 01:59:31 +0200

    login form

--- Aledrogo/resources/views/auth/login.blade.php
+++ Aledrogo/resources/views/auth/login.blade.php
@@ -1,24 +1,28 @@
 <x-layout>
 
-    <p>Helo to my Login Guide and Overview</p>
+    <p class="text-4xl m-8 text-center">Helo to my Login Guide and Overview</p>
 
-    <div>
+    <div class="m-8 border-2">
         <form action="{{ route('login') }}" method="POST">
             @csrf
-            <div>
+            <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                 <label for="email">Email</label>
                 <input type="email" name="email">
             </div>
-            <div>
+            <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                 <label for="password">Password</label>
                 <input type="password" name="password">
             </div>
+            <div class="max-w-screen-md items-center justify-between mx-auto p-4">
             <input type="checkbox" name="remember" id="remember">
-            <label for="remember"> remember me </label>
+            <label for="remember"> Remember me </label>
+            </div>
             @error('failed')
                     <p>{{ $message }}</p>
                 @enderror
-            <button>Login</button>
+            <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-4">    
+                <button class="text-xl content-center border-2 p-2">Login</button>
+            </div>
         </form>
     </div>
 
