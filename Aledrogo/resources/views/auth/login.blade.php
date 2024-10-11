e6a6cf0b127239f0c99548ea123cd6d2fa8ab58b
Kamil Kopischke <kamilkopischke@gmail.com>
2024-10-11 20:18:57 +0200

    5 linijek napisanych na zajeciach

--- Aledrogo/resources/views/auth/login.blade.php
+++ Aledrogo/resources/views/auth/login.blade.php
@@ -7,23 +7,26 @@
             @csrf
             <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                 <label for="email">Email</label>
-                <input type="email" name="email">
+                <input class="rounded" type="email" name="email">
             </div>
             <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                 <label for="password">Password</label>
-                <input type="password" name="password">
-            </div>
-            <div class="max-w-screen-md items-center justify-between mx-auto p-4">
-            <input type="checkbox" name="remember" id="remember">
-            <label for="remember"> Remember me </label>
+                <input class="rounded" type="password" name="password">
             </div>
             @error('failed')
                     <p>{{ $message }}</p>
                 @enderror
-            <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-4">    
+            <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-4">
                 <button class="text-xl content-center border-2 p-2">Login</button>
+                <div class="max-w-screen-md items-center justify-between mx-auto p-4">
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember"> Remember me </label>
+                </div>
             </div>
         </form>
+        <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-2">
+            <form action="{{route('login')}}"><button class="text-sm content-center p-0.5">i forgor 💀</button></form>
+        </div>
     </div>
 
 </x-layout>
--- Aledrogo/resources/views/auth/register.blade.php
+++ Aledrogo/resources/views/auth/register.blade.php
@@ -1,37 +1,37 @@
 <x-layout>
 
-    <p class="text-4xl m-8 text-center">Helo to my Register Guide and Overview</p>
+    <p class="text-4xl m-8 text-center">Strona rejestracji użytkownika</p>
 
     <div class="m-8">
         <form action="{{ route('register') }}" method="POST">
             @csrf
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <label for="name">Username</label>
-                    <input type="text" name="name" value="{{ old('name') }}">
+                    <input class="rounded" type="text" name="name" value="{{ old('name') }}">
                     @error('name')
                         <p>{{ $message }}</p>
                     @enderror
                 </div>
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <label for="email">Email</label>
-                    <input type="text" name="email" value="{{ old('email') }}">
+                    <input class="rounded" type="text" name="email" value="{{ old('email') }}">
                 </div>
                 @error('email')
                     <p class="text-amber-500 text-lg max-w-screen-md flex flex-wrap items-center justify-between mx-auto pl-8 pr-8 pb-2 pt-2">{{ $message }}</p>
                 @enderror
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <label for="password">Password</label>
-                    <input type="password" name="password">
+                    <input class="rounded" type="password" name="password">
                 </div>
                 @error('password')
                     <p class="text-amber-500 text-lg max-w-screen-md flex flex-wrap items-center justify-between mx-auto pl-8 pr-8 pb-2 pt-2">{{ $message }}</p>
                 @enderror
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between mx-auto p-4">
                     <label for="password_confirmation">Confirm Password</label>
-                    <input type="password" name="password_confirmation">
+                    <input class="rounded" type="password" name="password_confirmation">
                 </div>
                 <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-4">
-                    <button class="text-xl content-center border-2 p-2">Register</button>
+                    <button class="rounded-lg text-xl content-center border-2 p-2">Register</button>
                 </div>
         </form>
     </div>
