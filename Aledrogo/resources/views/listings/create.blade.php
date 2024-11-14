9997ab607fe51cf3cc774fc20b7f79534d8e9c00
Kamil Kopischke <kamilkopischke@gmail.com>
2024-11-14 22:52:06 +0100

    strona dodawania

--- Aledrogo/resources/views/listings/create.blade.php
+++ Aledrogo/resources/views/listings/create.blade.php
@@ -1,40 +1,36 @@
 <x-layout>
-
-    <div class="border-2" style="align-content: center; width:50%; min-width:max-content; margin:auto; margin-top:20px">
+    <div class="w-2/3 border-2 rounded ml-auto mr-auto mt-4">
         <h1 class="text-center text-3xl p-4 bg-blue-900">Add a new listing</h1>
     <form action="{{ route('listings.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="m-8">
-            <label for="title">Title</label>
-            <input type="text" name="title" value="{{ old('title') }}">
+        <div class="m-8 flex">
+            <label for="title" class="mr-2">Title&nbsp;</label><br>
+            <input class="border-blue-500 border-2 rounded basis-full" type="text" name="title" value="{{ old('title') }}">
             @error('title')
                 <p>{{ $message }}</p>
             @enderror
         </div>
         <div class="m-8 flex">
-            <label for="content" style="margin-right:10px;">Description</label>
-            <textarea class="text-black" rows="5" name="content">{{ old('content') }}</textarea>
+            <label for="content" class="mr-2">Price</label><br>
+            <input class="border-blue-500 border-2 rounded mb-auto w-28 mr-4" type="number" step="0.01" name="price" value="{{ old('title') }}">
             @error('content')
-                <p>{{ $message }}</p>
+            <p>{{ $message }}</p>
+            @enderror
+            <label for="content" class="mr-2">Description</label><br>
+            <textarea class="text-black border-blue-500 border-2 rounded basis-full" rows="5" name="content">{{ old('content') }}</textarea>
+            @error('content')
+            <p>{{ $message }}</p>
             @enderror
         </div>
         <div class="m-8">
-            <label for="file">Photo</label>
-            <input type="file" name="file" >
+            <label class="mr-2" for="file">Photo</label>
+            <input type="file" name="file" class="text-white">
             @error('file')
-                <p>{{ $message }}</p>
-            @enderror
-        </div>
-        <div class="m-8 flex">
-            <label for="content" style="margin-right:10px;">price</label>
-            <input type="number "step="0.01" name="price" value="{{ old('title') }}">
-            @error('content')
-                <p>{{ $message }}</p>
+                <p class="text-white">{{ $message }}</p>
             @enderror
         </div>
         <button class="border-2 rounded bg-indigo-900 flex" style="padding:5px 15px 5px 15px;margin:auto; margin-bottom:20px">Add</button>
     </form>
     <p>{{ session('succes') }}</p>
     </div>
-
 </x-layout>
