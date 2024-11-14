e670a18d02b3a714f736f051ad3d41170e8a9ecc
Kross003 <kacper07011@gmail.com>
2024-11-14 23:07:03 +0100

    Zmiany na stronie logowania i rejestracji
    Wprowadzenie graficznych zmian przycisku, padding pola tekstowego, reorganizacja klas css, QOL.

--- Aledrogo/resources/views/auth/login.blade.php
+++ Aledrogo/resources/views/auth/login.blade.php
@@ -1,32 +1,32 @@
 <x-layout>
 
 
-    <div class="items-center border-2" style="align-content: center; width:50%; min-width:max-content; margin:auto; margin-top:20px">
-        <div class=" p-8 w-max"style="width: max-content; margin:auto">
+    <div class="items-center border-2 content-center border-black w-6/12 min-w-max m-auto bg-sky-950">
+        <div class=" p-8 w-max m-auto">
             <p class="text-4xl m-8 text-center">Login</p>
             <form action="{{ route('login') }}" method="POST">
                 @csrf
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between p-4">
                     <label class="m-8" for="email">Email</label>
-                    <input class="rounded" type="email" name="email">
+                    <input class="rounded pl-2" type="email" name="email">
                 </div>
                 <div class="text-xl max-w-screen-md flex flex-wrap items-center justify-between p-4">
                     <label for="password">Password</label>
-                    <input class="rounded" type="password" name="password">
+                    <input class="rounded pl-2" type="password" name="password">
                 </div>
                 @error('failed')
                         <p>{{ $message }}</p>
                 @enderror
                 <div class="p-4">
                     <input type="checkbox" name="remember" id="remember">
-                    <label for="remember"> Remember me </label>
+                    <label for="remember" class="hover:italic"> Remember me </label>
                 </div>
                 <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-4">
-                    <button class="rounded-lg text-xl content-center border-2 p-2">Login</button>
+                    <button class="rounded-lg text-xl content-center border-2 p-2 hover:bg-blue-900 hover:border-black hover:text-black">Login</button>
                 </div>
             </form>
             <div class="max-w-screen-md flex flex-col flex-wrap items-center justify-between mx-auto p-2">
-                <form action="{{route('login')}}"><button class="text-sm content-center p-0.5">i forgor 💀</button></form>
+                <form action="{{route('login')}}"><button class="text-sm content-center p-0.5 hover:italic text-orange-400">i forgor hasła 💀</button></form>
             </div>
         </div>
     </div>
