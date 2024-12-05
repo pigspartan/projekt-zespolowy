0518ead6dd5a08739811b993c1b642450037d5e7
Kamil Kopischke <kamilkopischke@gmail.com>
2024-12-05 20:34:23 +0100

    lista zgłoszeń

--- Aledrogo/resources/views/auth/inspectUser.blade.php
+++ Aledrogo/resources/views/auth/inspectUser.blade.php
@@ -1,12 +1,1 @@
-<x-layout>
-
-{{$user->name}}
-
-@foreach($user->flaggedListings as $listing)
-                    <tr>
-                        <td>{{ $listing->title }}</td>
-                        <td>{{ $listing->pivot->reason }}</td>
-                    </tr>
-                @endforeach
-
-</x-layout>
+generalnie wymyśl co tu dać bo nikt nie wie
