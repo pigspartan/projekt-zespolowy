d6a018589742e747954c1076692849599b3831ec
Kross003 <kacper07011@gmail.com>
2024-12-06 01:11:23 +0100

    removed pride flag

--- Aledrogo/resources/views/suspended.blade.php
+++ Aledrogo/resources/views/suspended.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <h1 class="text-center text-3xl p-4 bg-pink-500">Your account has been suspended</h1>
-    <p class="text-center text-3xl p-4 bg-yellow-400">Contact with administrator to restore your account</p>
+    <h1 class="text-center text-3xl p-4 bg-sky-700">Your account has been suspended</h1>
+    <p class="text-center text-3xl p-4 bg-sky-700">Contact with administrator to restore your account</p>
     <p id="email" class="text-center text-3xl p-4 bg-sky-700">alledrogoteam@glorp.com</p>
 </x-layout>
